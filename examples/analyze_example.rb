37631a0a49eabfa0984e8f1c29efb3f91553005f
Javier Aguirre <contacto@javaguirre.net>
2012-07-06 12:18:55 +0200

    Changing 4 spaces to 2

--- examples/analyze_example.rb
+++ examples/analyze_example.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
-
 require "wappalyzer_rb"
+
 argument_url = ARGV[0]
 
 if not argument_url.nil?
-    puts WappalyzerRb::Detector.new(argument_url).analysis
+  puts WappalyzerRb::Detector.new(argument_url).analysis
 else
-    puts "Usage #{__FILE__} example.com"
+  puts "Usage #{__FILE__} example.com"
 end
